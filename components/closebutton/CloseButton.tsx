5539c71fed2c46bcc51349a2ee64806f55fa818d
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-05-12 22:39:06 +0200

    updated: closebutton, combobox

--- components/closebutton/CloseButton.tsx
+++ components/closebutton/CloseButton.tsx
@@ -6,8 +6,8 @@ import {ButtonProps} from "../button/Button";
 import {X} from "lucide-react";
 
 const CloseButton = React.forwardRef<HTMLDivElement, ButtonProps>(({ theme, className, ...props }) => (
-    <button className={cn("group bg-black hover:bg-dark rounded-lg", className)} {...props}>
-        <X size={16} className={"text-gray group-hover:text-white m-1"}/>
+    <button className={cn("group/close bg-black hover:bg-dark rounded-lg", className)} {...props}>
+        <X size={16} className={"text-gray group-hover/close:text-white m-1"}/>
     </button>
 ));
 CloseButton.displayName = "CloseButton";
