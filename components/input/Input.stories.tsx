0276a9476d247e86a3c54affb9bda0b3733044ea
marius <marius.ahsmus@gmail.com>
2024-05-12 11:39:14 +0200

    updated: input variants

--- components/input/Input.stories.tsx
+++ components/input/Input.stories.tsx
@@ -18,7 +18,7 @@ type Story = StoryObj<typeof Input>
 export const Default: Story = {
     render: () => {
         return (
-            <Input placeholder="Small input" label={"Label Title"}>
+            <Input placeholder="Small input" label={"Label Title"} border={"none"}>
             </Input>
         );
     },
