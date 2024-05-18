ba452cd90c0dce61a346544cf0023791b8137bca
marius <marius.ahsmus@gmail.com>
2024-05-18 17:41:06 +0200

    feat: breadcrumb adjustments

--- components/breadcrump/Breadcrump.tsx
+++ components/breadcrump/Breadcrump.tsx
@@ -1,41 +1,27 @@
 "use client";
 
-import { cva, VariantProps } from "class-variance-authority";
-import React from "react";
+import React, {forwardRef} from "react";
 import { cn } from "../../utils/cn";
 import {ChevronRight} from "lucide-react";
 
-const breadcrump = cva("w-max rounded-lg font-normal bg-opacity-20 bg-black text-placeholder " +
-    "flex items-center text-sm py-1 px-4 border border-white border-opacity-20");
-
-interface BreadcrumpProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof breadcrump> {
+interface BreadcrumpProps extends React.HTMLAttributes<HTMLDivElement> {
     pastText?: string;
     nowText?: string;
 }
 
-const BreadcrumpPastItem = React.forwardRef<HTMLDivElement, BreadcrumpProps>(({ pastText, className, ...props }, ref) => (
-    <div className={cn("cursor-pointer hover:text-white hover:underline", className)} ref={ref} {...props}>
+const Breadcrump = forwardRef<HTMLDivElement, BreadcrumpProps>(({ pastText, nowText, className, ...props }, ref) => (
+    <div className={cn("w-max rounded-lg font-normal bg-opacity-20 bg-black text-placeholder " +
+        "flex flex-row text-xs border border-white border-opacity-20 px-2 py-1 items-center", className)}
+         ref={ref} {...props}>
+        <span className={cn("cursor-pointer hover:text-white hover:bg-selected rounded-md p-1", className)} ref={ref} {...props}>
         {pastText}
-    </div>
-));
-BreadcrumpPastItem.displayName = "BreadcrumpPastItem";
-
-
-const BreadcrumpNowItem = React.forwardRef<HTMLDivElement, BreadcrumpProps>(({ nowText, className, ...props }, ref) => (
-    <div className={cn("text-gray", className)} ref={ref} {...props}>
+        </span>
+        <ChevronRight strokeWidth={2} size={15} color={"gray"} className={"mx-2"}/>
+        <span className={cn("text-gray", className)} ref={ref} {...props}>
         {nowText}
-    </div>
-));
-BreadcrumpNowItem.displayName = "BreadcrumpNowItem";
-
-
-const Breadcrump = React.forwardRef<HTMLDivElement, BreadcrumpProps>(({ pastText, nowText, ...props }, ref) => (
-    <div className={cn(breadcrump({ }))} ref={ref} {...props}>
-        <BreadcrumpPastItem pastText={pastText}/>
-        <ChevronRight strokeWidth={4} size={15} color={"gray"} className={"m-2"}/>
-        <BreadcrumpNowItem nowText={nowText}/>
+        </span>
     </div>
 ));
 Breadcrump.displayName = "Breadcrump";
 
-export { Breadcrump, BreadcrumpPastItem, BreadcrumpNowItem };
+export {Breadcrump };
