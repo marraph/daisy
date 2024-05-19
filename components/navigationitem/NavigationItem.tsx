8d4f20d382687dce7cee41df9f993ebebe53d809
marius <marius.ahsmus@gmail.com>
2024-05-19 11:51:08 +0200

    fix: navigationItem adjustment

--- components/navigationitem/NavigationItem.tsx
+++ components/navigationitem/NavigationItem.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import React, {ReactNode, useState} from "react";
+import React, {ReactNode} from "react";
 import {cn} from "../../utils/cn";
 
 interface NavigationItemProps extends React.HTMLAttributes<HTMLDivElement> {
     title: string;
     icon: ReactNode;
+    selected: boolean;
 }
 
-const NavigationItem: React.FC<NavigationItemProps> = ({ title, icon, ...props }) => {
-    const [selected, setSelected] = useState(false);
-
+const NavigationItem: React.FC<NavigationItemProps> = ({ title, icon, selected, ...props }) => {
     return (
         <div className={selected ?
             "text-sm rounded-lg font-normal cursor-pointer flex items-center " +
             "bg-selected text-white border border-white border-opacity-20" :
             "bg-black text-gray text-md rounded-lg font-medium cursor-pointer " +
             "flex items-center hover:bg-selected hover:text-white"}
-            style={{width: 240}} onClick={() => setSelected(true)}
+            style={{width: 240}}
             {...props}>
             <div className={cn("m-2 ml-4 mr-2")}>
                 {icon}
