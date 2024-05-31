20826b729470510244121d6a3c11cd9e7d682ea1
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-05-31 20:01:32 +0200

    feat: created filterComponent, reverted contextMenu

--- components/contextmenu/ContextMenu.tsx
+++ components/contextmenu/ContextMenu.tsx
@@ -4,14 +4,9 @@ import React, {ReactNode, useState} from "react";
 import {cn} from "../../utils/cn";
 import {Check} from "lucide-react";
 
-interface ContextMenuProps extends React.HTMLAttributes<HTMLDivElement> {
-    checkmarkOn: boolean;
-}
-
 interface ContextMenuItemProps extends React.HTMLAttributes<HTMLDivElement> {
     title: string;
     shortcut?: string;
-    isSelected?: boolean;
     onClick?: () => void;
 }
 
@@ -33,11 +28,9 @@ const ContextMenuIcon = React.forwardRef<HTMLDivElement, ContextMenuIconProps>((
 ContextMenuIcon.displayName = "ContextMenuIcon";
 
 
-const ContextMenuItem = React.forwardRef<HTMLDivElement, ContextMenuItemProps>(({ isSelected, title, shortcut, className, ...props }, ref) => (
-    <div className={cn(isSelected ? "bg-dark text-white group cursor-pointer rounded-lg hover:bg-selected hover:text-white p-2 flex flex-row justify-between items-center" :
-        "bg-black group cursor-pointer rounded-lg hover:bg-selected hover:text-white p-2 flex flex-row justify-between items-center", className)} ref={ref} {...props}>
+const ContextMenuItem = React.forwardRef<HTMLDivElement, ContextMenuItemProps>(({ title, shortcut, className, ...props }, ref) => (
+    <div className={cn("bg-black group cursor-pointer rounded-lg hover:bg-selected hover:text-white p-2 flex flex-row justify-between items-center", className)} ref={ref} {...props}>
         <div className={"flex flex-row items-center space-x-2"}>
-            {isSelected && <Check size={12} strokeWidth={3} className={"mr-2"} />}
             {props.children}
             {title}
         </div>
@@ -51,9 +44,7 @@ const ContextMenuItem = React.forwardRef<HTMLDivElement, ContextMenuItemProps>((
 ContextMenuItem.displayName = "ContextMenuItem";
 
 
-const ContextMenu = React.forwardRef<HTMLDivElement, ContextMenuProps>(({ checkmarkOn, className, ...props }, ref) => {
-    const [selectedValue, setSelectedValue] = useState<null | string>(null);
-
+const ContextMenu = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => {
     return (
         <div
             className={cn("w-full rounded-lg font-normal py-2 bg-black text-gray border border-white border-opacity-20", className)}
@@ -63,9 +54,7 @@ const ContextMenu = React.forwardRef<HTMLDivElement, ContextMenuProps>(({ checkm
                     return React.cloneElement(child, {
                         onClick: () => {
                             child.props.onClick && child.props.onClick();
-                            setSelectedValue(child.props.title);
                         },
-                        isSelected: checkmarkOn && selectedValue === child.props.title,
                     });
                 }
                 return child;
