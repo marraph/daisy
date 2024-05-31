fc1daf652b663cef8b43e5df89f49c02c92e20c5
marius <marius.ahsmus@gmail.com>
2024-05-31 22:54:55 +0200

    feat: filter tabs

--- components/filter/Filter.tsx
+++ components/filter/Filter.tsx
@@ -10,25 +10,38 @@ interface FilterItemProps extends React.HTMLAttributes<HTMLDivElement> {
     onClick?: () => void;
     data?: string[];
     title: string;
+    isOpen?: boolean;
+    onOpen?: () => void;
+    onClose?: () => void;
 }
 
 
-const FilterItem = React.forwardRef<HTMLDivElement, FilterItemProps>(({ title, data, className, ...props }, ref) => {
-    const [isOpen, setIsOpen] = useState(false);
+const FilterItem = React.forwardRef<HTMLDivElement, FilterItemProps>(({ title, data, isOpen, onOpen, onClose, className, ...props }, ref) => {
+    const toggleOpen = () => {
+        if (isOpen) {
+            onClose();
+        } else {
+            onOpen();
+        }
+    };
 
     return (
-        <div className={"cursor-pointer text-gray hover:text-white hover:bg-dark mx-2 my-1 rounded-lg "} onClick={() => setIsOpen(true)}>
-            <span className={"px-2 py-1"}>{title}</span>
+        <div className={"relative"}>
+            <div className={"cursor-pointer text-gray text-sm hover:text-white hover:bg-dark mx-2 my-1 rounded-lg "}
+                 onClick={() => toggleOpen()}>
+                <span className={"px-2 py-1"}>{title}</span>
+            </div>
             {isOpen &&
-                <div className={"bg-black rounded-lg border border-white border-opacity-20"}>
+                <div className={"absolute left-full top-0 ml-2 py-2 space-y-1 w-max bg-black rounded-lg border border-white border-opacity-20"}>
                     {data.map((title) => (
-                        <div key={title} className={"p-2"}>
-                            <span>{title}</span>
+                        <div key={title} className={"mx-2 my-1 text-gray text-sm rounded-lg cursor-pointer hover:text-white hover:bg-dark"}>
+                            <span className={"px-2 py-1"}>{title}</span>
                         </div>
                     ))}
                 </div>
             }
         </div>
+
     );
 });
 FilterItem.displayName = "FilterItem";
@@ -37,6 +50,7 @@ FilterItem.displayName = "FilterItem";
 const Filter = React.forwardRef<HTMLDivElement, FilterProps>(({ className, ...props }, ref) => {
     const [filterList, setFilterList] = useState<string[]>([]);
     const [showFilter, setShowFilter] = useState(false);
+    const [openItem, setOpenItem] = useState<string | null>(null);
 
     const deleteFilter = () => {
         setFilterList([]);
@@ -45,6 +59,7 @@ const Filter = React.forwardRef<HTMLDivElement, FilterProps>(({ className, ...pr
 
     const closeMenus = () => {
         setShowFilter(false);
+        setOpenItem(null);
     }
 
     return (
@@ -66,16 +81,21 @@ const Filter = React.forwardRef<HTMLDivElement, FilterProps>(({ className, ...pr
                         deleteFilter();
                     }}/>}
             </button>
-                <div className={"bg-black rounded-lg border border-white border-opacity-20 py-2"}>
-                    {showFilter &&
-                        React.Children.map(props.children, (child) => {
+                {showFilter &&
+                    <div className={"bg-black rounded-lg border border-white border-opacity-20 py-2"}>
+                        {React.Children.map(props.children, (child) => {
                             if (React.isValidElement<FilterItemProps>(child)) {
                                 return React.cloneElement(child, {
+                                    isOpen: openItem === child.props.title,
+                                    onOpen: () => setOpenItem(child.props.title),
+                                    onClose: () => setOpenItem(null),
                                 });
                             }
                             return child;
-                    })}
-                </div>
+                        })}
+                    </div>
+                }
+
         </div>
     )
 
