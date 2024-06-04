e7016106d7ece71438db6d70259176c49388c1e4
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-06-04 17:38:47 +0200

    feat: tried dialog fix

--- components/dialog/Dialog.stories.tsx
+++ components/dialog/Dialog.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Dialog, DialogSeperator} from './Dialog';
+import React, {useRef, useState} from 'react';
+import {Dialog} from './Dialog';
 import {Meta, StoryObj} from "@storybook/react";
 import { Button } from '../button/Button';
 import { Input } from '../input/Input';
@@ -18,15 +18,15 @@ export default meta;
 type Story = StoryObj<typeof Dialog>;
 
 export const Default = () => {
-    let dialogRef = React.useRef<HTMLDialogElement>(null);
+
+    const dialogRef = useRef<HTMLDialogElement | null>(null);
 
     return (
         <>
-            <button className={"bg-black text-white p-2 text-base rounded-lg border border-white border-opacity-20"} onClick={dialogRef.current?.showModal}>Dialog</button>
-            <Dialog className={"fixed"} ref={dialogRef}>
+            <button className={"bg-black text-white p-2 text-base rounded-lg border border-white border-opacity-20"} onClick={() => dialogRef?.current.showModal()}>Dialog</button>
+            <Dialog ref={dialogRef}>
                 <Button text={"Button"}></Button>
                 <Input placeholder={"placeholder"}></Input>
-                <DialogSeperator />
                 <Button text={"Button"}></Button>
                 <Input placeholder={"placeholder"}></Input>
             </Dialog>
--- components/dialog/Dialog.tsx
+++ components/dialog/Dialog.tsx
@@ -3,18 +3,15 @@
 import React from "react";
 import {cn} from "../../utils/cn";
 
-const DialogSeperator = React.forwardRef<HTMLHRElement, React.HTMLAttributes<HTMLHRElement>>(({ className, ...props }, ref) => (
-    <hr className={cn("flex-grow text-white text-opacity-20")} ref={ref} {...props} />
-));
-DialogSeperator.displayName = "DialogSeperator";
+const Dialog = React.forwardRef<HTMLDialogElement, React.DialogHTMLAttributes<HTMLDialogElement>>(({ className, ...props }, ref) => {
+    return (
+        <dialog className={cn("group backdrop:bg-black/60 rounded-lg bg-black backdrop backdrop-opacity-20 backdrop-brightness-0", className)}{...props} ref={ref}>
+            {props.children}
+        </dialog>
+    );
 
 
-const Dialog = React.forwardRef<HTMLDialogElement, React.DialogHTMLAttributes<HTMLDialogElement>>(({ className, ...props }, ref) => (
-    <dialog className={cn("group backdrop:bg-black/60 rounded-lg bg-black backdrop backdrop-opacity-20 backdrop-brightness-0" , className)} ref={ref} {...props}>
-        {props.children}
-    </dialog>
-
-));
+});
 Dialog.displayName = "Dialog";
 
-export { Dialog, DialogSeperator };
+export {Dialog};
