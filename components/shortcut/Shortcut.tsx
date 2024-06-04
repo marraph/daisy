7bb9fc62b140b6e517c3a6d69aa82f7399538b70
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-06-04 14:26:49 +0200

    fix: shortcut children

--- components/shortcut/Shortcut.tsx
+++ components/shortcut/Shortcut.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 export interface ShortcutProps extends React.HTMLAttributes<HTMLDivElement> {
-    text: string;
+    text?: string;
 }
 
 const Shortcut = React.forwardRef<HTMLDivElement, ShortcutProps>(({ text, className, ...props }, ref) => (
         <div className={"flex items-center rounded-md border border-white border-opacity-15 shadow-[inset_0_-2px_4px_rgba(0,0,0,0.6)] bg-dark h-min text-placeholder text-xs px-1 py-0.5"} ref={ref} {...props}>
-            <span>{text}</span>
+            {props.children}
+            {text && <span>{text}</span>}
         </div>
     ),
 );
