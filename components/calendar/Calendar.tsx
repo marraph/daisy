b407eb92d86fcffab1b4667bbe7d830c3abf75cf
marius <marius.ahsmus@gmail.com>
2024-07-04 10:14:33 +0200

    feat: calendar style

--- components/calendar/Calendar.tsx
+++ components/calendar/Calendar.tsx
@@ -16,7 +16,7 @@ const Calendar: React.FC<CalendarProps> = ({ className, classNames, ...props }) 
             selected={selectedDay}
             onSelect={setSelectedDay}
             showOutsideDays={true}
-            className={cn("p-3 text-white bg-black rounded-lg border border-white border-opacity-20", className)}
+            className={"p-3 text-white bg-black rounded-lg border border-white border-opacity-20"}
             classNames={{
                 months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
                 month: "space-y-4",
@@ -28,20 +28,14 @@ const Calendar: React.FC<CalendarProps> = ({ className, classNames, ...props }) 
                 nav_button_next: "absolute right-1",
                 table: "w-full border-collapse space-y-1",
                 head_row: "flex",
-                head_cell:
-                    "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+                head_cell: "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
                 row: "flex w-full mt-2",
-                cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-                day: cn("h-9 w-9 p-0 font-normal aria-selected:opacity-100 cursor-pointer rounded-lg hover:bg-dark"),
-                day_range_end: "day-range-end",
-                day_selected:
-                    "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-                day_today: "bg-dark text-white rounded-lg hover:text-black hover:bg-white",
-                day_outside:
-                    "day-outside opacity-50 aria-selected:bg-accent/50 aria-selected:opacity-30",
+                cell: "h-9 w-9 text-center text-sm p-0 relative focus-within:relative focus-within:z-20",
+                day: "h-9 w-9 p-0 font-normal cursor-pointer rounded-lg hover:bg-dark",
+                day_today: "bg-dark text-white rounded-lg",
+                day_outside: "day-outside opacity-50",
+                day_selected: "bg-white text-black",
                 day_disabled: "text-muted-foreground opacity-50",
-                day_range_middle:
-                    "aria-selected:bg-accent aria-selected:text-accent-foreground",
                 day_hidden: "invisible",
                 ...classNames,
             }}
--- components/daterangepicker/DateRangePicker.tsx
+++ components/daterangepicker/DateRangePicker.tsx
@@ -126,21 +126,17 @@ const DateRangePicker = forwardRef<DateRangePickerRef, DateRangePickerProps>(({d
                                    nav_button_next: "absolute right-1",
                                    table: "w-full border-collapse space-y-1",
                                    head_row: "flex",
-                                   head_cell:
-                                       "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+                                   head_cell: "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
                                    row: "flex w-full mt-2",
-                                   cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
-                                   day: cn("h-9 w-9 p-0 font-normal aria-selected:opacity-100 cursor-pointer rounded-lg hover:bg-dark"),
+                                   cell: "h-9 w-9 text-center text-sm p-0 relative focus-within:relative focus-within:z-20",
+                                   day: cn("h-9 w-9 p-0 font-normal cursor-pointer rounded-lg hover:bg-dark"),
                                    day_range_end: "day-range-end rounded-r-lg rounded-l-none",
                                    day_range_start: "rounded-l-lg rounded-r-none",
-                                   day_selected:
-                                       "bg-dark text-white hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-                                   day_today: "bg-selectwhite text-dark rounded-lg hover:text-black hover:bg-white",
-                                   day_outside:
-                                       "day-outside opacity-50 aria-selected:bg-accent/50 aria-selected:opacity-30",
+                                   day_selected: "bg-selectwhite text-dark hover:bg-selectwhite rounded-lg",
+                                   day_today: "bg-dark text-white rounded-lg",
+                                   day_outside: "day-outside opacity-50",
                                    day_disabled: "text-muted-foreground opacity-50",
-                                   day_range_middle:
-                                       "aria-selected:bg-dark text-white rounded-none",
+                                   day_range_middle: "text-white rounded-none",
                                    day_hidden: "invisible",
                                }}
                     />
